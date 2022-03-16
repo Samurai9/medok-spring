8c83a12001f55498b353c23f46f1cbcb374ba57d
Erik <naserik99@mail.ru>
2022-03-16 18:17:41 +0300

    Change product rest controller

--- src/main/java/ru/kpfu/itis/nasibullin/medokspring/controllers/rest/RestProductController.java
+++ src/main/java/ru/kpfu/itis/nasibullin/medokspring/controllers/rest/RestProductController.java
@@ -7,43 +7,45 @@ import ru.kpfu.itis.nasibullin.medokspring.entities.Product;
 import ru.kpfu.itis.nasibullin.medokspring.entities.ProductCategory;
 import ru.kpfu.itis.nasibullin.medokspring.services.product.ProductService;
 
+import java.util.List;
+
 @RestController
 public class RestProductController {
     @Autowired
     private ProductService productService;
 
     @GetMapping("/rest/product/{id}")
-    public ResponseEntity getProduct(@PathVariable int id) {
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
         try {
-            return ResponseEntity.ok(productService.findById(id).toString());
+            return ResponseEntity.ok(productService.findById(id));
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @GetMapping("/rest/product/liquid")
-    public ResponseEntity getAllLiquidProducts() {
+    public ResponseEntity<List<Product>> getAllLiquidProducts() {
         return ResponseEntity.ok(productService.findAllByCategory(ProductCategory.LIQUID));
     }
 
     @GetMapping("/rest/product/honeycomb")
-    public ResponseEntity getAllHoneycombProducts() {
+    public ResponseEntity<List<Product>> getAllHoneycombProducts() {
         return ResponseEntity.ok(productService.findAllByCategory(ProductCategory.HONEYCOMB));
     }
 
     @PutMapping("/rest/product/")
-    public ResponseEntity addProduct(@RequestBody Product product) {
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
         return ResponseEntity.ok(productService.save(product));
     }
 
     @DeleteMapping("/rest/product/{id}")
-    public ResponseEntity deleteProduct(@PathVariable int id) {
+    public ResponseEntity<Product> deleteProduct(@PathVariable int id) {
         productService.delete(id);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/rest/product/{id}")
-    public ResponseEntity updateProduct(@PathVariable int id, @RequestBody Product product) {
+    public ResponseEntity<Product> updateProduct(@PathVariable int id, @RequestBody Product product) {
         try {
             product.setProductId(id);
             return ResponseEntity.ok(productService.update(product));
