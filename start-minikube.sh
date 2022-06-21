a485fba2d155d0a22b2e203420f0c382738c359e
Erik Nasibullin <nasibullinei@internet.ru>
2022-06-21 12:23:37 +0300

    Add k8s support

--- start-minikube.sh
+++ start-minikube.sh
@@ -1,5 +1,7 @@
 #Start minikube
-minikube start --mount-string ${PWD}/configs/promtail/:/etc/promtail/ --mount
+minikube start --mount-string ${PWD}/configs/:/configs/ --mount
+#Create namespace
+kubectl create namespace medok
 #Apply configs
 kubectl apply --namespace medok -f ./configs/kube_configs
 #Open dashboard to background
